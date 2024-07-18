5d6037b47ba88813bc2394f077560a69aa3ad98b
Niraj <niraj@Quantum.Compiler>
2024-07-18 17:07:52 +0530

    Changed by me

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'The project  Avalon'),
+      home: const MyHomePage(title: 'Fuk you aditya'),
     );
   }
 }
