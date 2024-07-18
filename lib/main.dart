e7a98b0a3c53a4a0dcd8c0b951ca017563e87bc5
Aditya Sawant <sawantaditya3636@gmail.com>
2024-07-18 18:21:03 +0530

    fourth

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Avalon Project',
+      title: 'Avalon Project Name',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
