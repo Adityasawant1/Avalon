e25afdcf0aed7aff379e8817fe6ebefe44b20902
Aditya Sawant <sawantaditya3636@gmail.com>
2024-07-19 10:28:58 +0530

    logini mage add colors setup

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Avalon ',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(),
       home: const LoginPage(),
     );
