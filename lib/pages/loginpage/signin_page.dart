966fc126b6cae9f88a84603ebf70d43f6b26f3ca
Niraj Khetre <khetreniraj@gmail.com>
2024-07-20 18:31:55 +0530

    add recover password button

--- lib/pages/loginpage/signin_page.dart
+++ lib/pages/loginpage/signin_page.dart
@@ -1,14 +1,15 @@
 import 'package:avalon/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SignInPage extends StatelessWidget {
   const SignInPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-      Size size = MediaQuery.of(context).size;
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-       body:Container(
+      body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
             begin: Alignment.topCenter,
@@ -16,76 +17,100 @@ class SignInPage extends StatelessWidget {
               backgroundColor2,
               backgroundColor2,
               backgroundColor4,
-            ]
-          )
+            ],
           ),
-          child: ListView(
-            children:[
-              SizedBox(height: size.height * 0.1,),
-              Container(
-                child: Column(
-                  children: [
-                    Text('Welocme Back!',
+        ),
+        child: ListView(
+          children: [
+            SizedBox(height: size.height * 0.1,),
+            Container(
+              child: Column(
+                children: [
+                  // Title
+                  Text(
+                    'Welcome Back!',
                     style: TextStyle(
-                        fontWeight: FontWeight.w700,
-                        fontSize: 39,
-                        color: textColor1,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 39,
+                      color: textColor1,
                     ),
-                    ),
-                    Text('Continue your journey towards a\n sustainable future',
+                  ),
+                  // Subtitle
+                  Text(
+                    'Continue your journey towards a\n sustainable future',
                     textAlign: TextAlign.center,
                     style: TextStyle(
                       fontSize: 20,
                       color: textColor2,
                     ),
+                  ),
+                  SizedBox(height: size.height * 0.1,),
+                  Container(
+                    width: size.width * 0.8,
+                    child: Column(
+                      children: [
+                        // Enter Email Input Field
+                        TextField(
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(
+                              vertical: 18,
+                              horizontal: 22,
+                            ),
+                            hintText: "Enter Email",
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: size.height * 0.03,),
+                        // Password Input Text Field
+                        TextField(
+                          decoration: InputDecoration(
+                            contentPadding: const EdgeInsets.symmetric(
+                              vertical: 18,
+                              horizontal: 22,
+                            ),
+                            hintText: "Enter Password",
+                            suffixIcon: IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.visibility_off),
+                            ),
+                            fillColor: Colors.white,
+                            filled: true,
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                              borderSide: BorderSide.none,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: size.height * 0.02,),
+                        // Recover Password Button
+                        Align(
+                          alignment: Alignment.centerRight,
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Text(
+                              "Recover Password",
+                              style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.bold,
+                                color: textColor2,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
-                    SizedBox(height: size.height * 0.1,),
-                    Container(
-                      width: size.width * 0.8,
-                      child:  Column(
-                        children: [
-                          TextField(
-                            decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.symmetric(
-                                vertical: 15, 
-                                horizontal: 22),
-                              hintText: "Enter Email",
-                              fillColor: Colors.white,
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide:BorderSide.none,
-                              )
-                            ),
-                          ),
-                          SizedBox(height: size.height * 0.03 ,),
-                          //Password Input Text field 
-                          TextField(
-                            decoration: InputDecoration(
-                              contentPadding: const EdgeInsets.symmetric(
-                                vertical: 18, 
-                                horizontal: 22),
-                              hintText: "Enter Password",
-                              suffixIcon: const Icon(Icons.visibility_off),
-                              suffixIconColor: Colors.black26,
-                              fillColor: Colors.white,
-                              filled: true,
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(10),
-                                borderSide:BorderSide.none,
-                              )
-                            ),
-                          ),
-                        ],
-                      ),
-                    )
-                  ] 
-                ),
-              )
-             ] 
-
-          )
-        )
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
